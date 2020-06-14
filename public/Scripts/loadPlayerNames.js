3565d8cb3e7e17f4b2ebd96eba34a7e0775baec7
darcese <spartans991@yahoo.com>
2020-06-14 12:07:45 -0700

    shared client server deploy

---
+++ public/Scripts/loadPlayerNames.js
@@ -0,0 +1,42 @@
+var input = document.getElementById("player-input-name");
+var dataList = document.getElementById("player-list-input");
+// Create a new XMLHttpRequest.
+var request = new XMLHttpRequest();
+
+// Handle state changes for the request.
+request.onreadystatechange = function(response) {
+  if (request.readyState === 4) {
+    if (request.status === 200) {
+      // Parse the JSON
+      var jsonOptions = JSON.parse(request.responseText);
+
+      // Loop over the JSON array.
+      jsonOptions.forEach(function(item) {
+        // Create a new <option> element.
+        var option = document.createElement('option');
+        // Set the value using the item in the JSON array.
+        option.value = item.name;
+        // Add the <option> element to the <datalist>.
+        dataList.appendChild(option);
+      });
+
+      // Update the placeholder text.
+      input.placeholder = "player first & last name";
+      
+    } else {
+      // An error occured :(
+      input.placeholder = "Couldn't load datalist options :(";
+    }
+  }
+};
+
+// Update the placeholder text.
+input.placeholder = "Loading options...";
+
+// Set up and make the request.
+request.open('GET', 'Scripts/player_list.json', true);
+request.send();
+
+// START THE page with the focus on the player-input-name
+document.getElementById("player-input-name").focus();
+//////////////////////////////////////////////////////////////////
---
+++ public/Scripts/sendPredictByStats.js
@@ -0,0 +1,55 @@
+
+function sendInputToApi(){
+   
+   
+    
+
+    let stats = ['Total Years',  'MVPs', 'Pro Bowls', 'All Pros', 'SB Wins', 'AP POYs'];
+    let table = document.getElementById("predict-by-stats-table");
+    statInputIds = []
+    stats.forEach(element => {
+      statInputIds.push(element.replace(/\s/g, ''))
+    });
+
+    
+    var ApiObject = {};
+    statInputIds.forEach(element => { ApiObject[element] = document.getElementById(element).value || 0;} );
+    // var ApiObject = {
+    //                 Position: ApiValuesAsArray[0].toUpperCase() || 'QB',
+    //                 TotalYears: ApiValuesAsArray[1] || 0,
+    //                 MVP: ApiValuesAsArray[2] || 0 ,
+    //                 ProBowl: ApiValuesAsArray[3] || 0,
+    //                 AllPro: ApiValuesAsArray[4] || 0,
+    //                 SbChamp: ApiValuesAsArray[5] || 0,
+    //                 APpoy: ApiValuesAsArray[6] || 0,
+    //                 };
+    ApiObject['Position'] = document.getElementById("position-select").value; 
+    ApiObject = JSON.stringify(ApiObject);
+  
+    var req = new XMLHttpRequest();   // new HttpRequest instance 
+
+    req.open("POST", 'https://nfl-hall-of-fame-predictor-api.herokuapp.com/api/inputs_as_stats/generate_prediction', true); //make this False later to try synchronous version
+    req.setRequestHeader("Content-Type", "application/json"); //might not be necessary for now
+    req.send(ApiObject);
+  
+    req.onreadystatechange = function(response) {
+      if (req.readyState === 4) {
+        if (req.status === 200) {
+          // Parse the JSON
+          
+          document.getElementById( "api-test-response-label").style.display= "flex";
+          document.getElementById("api-test-response-label").innerHTML =   Number(Number(req.responseText).toFixed(2)* 100 ) + '%';
+          window.scrollBy(0, 100);
+       
+        }
+      }
+    };
+    
+    //////////let ApiObject1 = document.getElementById("position-select").value
+  
+  }
+    
+document.getElementById("player-input-stats-predict").addEventListener('click', sendInputToApi, false);
+  //document.getElementById("player-input-name").addEventListener('focus', (event) => {
+   // document.getElementById("player-input-name").style.color= 'pink';    
+  //});
